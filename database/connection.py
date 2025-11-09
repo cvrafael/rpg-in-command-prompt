d1164f7c6db6c5f00e6366701b6590f77fa8dc72
cvrafael <caio-victor-rafael@hotmail.com>
2025-11-09 11:07:24 -0300

    :lock: Implemented dotenv to the postgres connections

--- database/connection.py
+++ database/connection.py
@@ -1,4 +1,9 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class ConnectionPostgreSQL():
     def __init__(self):
         pass
@@ -6,11 +11,11 @@ class ConnectionPostgreSQL():
     @property
     def connectPostgress(self):
 
-        conn = psycopg2.connect(database="rpg", 
-                                user = "postgres", 
-                                host = "172.18.0.2",
-                                password = "catsarecool",
-                                port = 5432)
+        conn = psycopg2.connect(database=os.getenv('DATABASE'), 
+                                user = os.getenv('USERNAME'), 
+                                host = os.getenv('HOST'),
+                                password = os.getenv('PASSWORD'),
+                                port = os.getenv('PORT'))
         try:
             cur = conn.cursor()
             # Execute a command: create datacamp_courses table
